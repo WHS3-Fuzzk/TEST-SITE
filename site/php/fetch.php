6f5250d67a6715428c38ca6f6830606f08f7ce26
sub0810 <70949965+h4vrut4@users.noreply.github.com>
2025-07-26 19:00:19 +0900

    feat: php ssrf 페이지수정
    file_get_contents는 문자 그대로 처리하기 때문에, http://를 붙여주는 로직을 추가하여
    ssrf 취약점이 발생할 수 있도록 수정.

--- site/php/fetch.php
+++ site/php/fetch.php
@@ -2,6 +2,10 @@
 $response = '';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $url = $_POST['url'];
+    // http:// 또는 https://가 없으면 자동으로 붙임
+    if (!preg_match('#^https?://#i', $url)) {
+        $url = 'http://' . $url;
+    }
     $response = @file_get_contents($url);
 }
 ?>
@@ -11,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
     <h2>SSRF 테스트</h2>
     <form method="post">
-        URL: <input type="text" name="url">
+        URL: <input type="text" name="url" placeholder="e.g. 127.0.0.1:80 or http://site.com">
         <button type="submit">요청</button>
     </form>
     <pre><?= htmlspecialchars($response) ?></pre>
